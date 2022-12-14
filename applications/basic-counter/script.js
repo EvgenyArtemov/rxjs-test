9555aff077dc05e78a34905d7b1c9b743e254250
Evgeny Artemov <eartemov013@gmail.com>
2022-12-14 13:13:38 +0400

    add fromEvent exercise

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,5 +1,16 @@
 import { fromEvent, interval, merge, NEVER } from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
+const interval$ = interval(1000);
+
 const start$ = fromEvent(startButton, 'click');
+let subscription;
+start$.subscribe(() => {
+  subscription = interval$.subscribe(val => {
+    setCount(val+1);
+  })
+})
 const pause$ = fromEvent(pauseButton, 'click');
+pause$.subscribe(() => {
+  subscription.unsubscribe();
+})
